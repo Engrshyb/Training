44addbb7e05e10e58ac6cff7b04e5d193dbbe288
Engrshyb <engrshyb@gmail.com>
2025-11-12 05:26:39 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //uses newer version js code only
+
+//alert("hello", 3+3) //alert doesnt work in node
+
+let name = "shoaib"
+
+let age = 21    //range of number datatype is from 2-2power of 53 for more bigger number use "bigInt"
+
+let isLoggedIn = true
+
+let  state = null
+
+
+// following are datatypes
+// Number       simple number upto2 power(53)
+// BigInt       number bigger than 2power(53)
+// String       "any combination of characters"
+// Boolean      true/false
+// null         "it isnt 0"
+// undefined    
+// symbol       unique
+
+//object        
+
+console.log (typeof null)
+console.log (typeof undefined)
