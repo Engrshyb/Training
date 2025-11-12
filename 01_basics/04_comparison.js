1dca9ab672a8894cf657d1e64a9ae4ffb05eb607
Engrshyb <engrshyb@gmail.com>
2025-11-12 07:28:26 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+console.log(null>0)
+console.log(null==0)    //<,> converts null into number and then compare it while == doesn't  
+console.log(null<=0)
+
+
+console.log(undefined>0)
+console.log(undefined==0)    
+console.log(undefined<0)
