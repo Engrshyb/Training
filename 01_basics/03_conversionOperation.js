c81bae5fe520bdcbde10a9aea281ae6c75cc755f
Engrshyb <engrshyb@gmail.com>
2025-11-12 06:04:30 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+                                    //NUMBER TO STRING CONVERSION
+// let score1 = "33abc"
+
+// let x =Number(score1)
+
+// console.log(typeof x)
+
+// console.log (typeof score1)
+
+// console.log(score1)
+
+// console.log(x)
+
+                                    //BOOLEAN Conversion
+        //FOR NUMBER
+// let isLoggedIn = 0
+// let x=Boolean(isLoggedIn)
+// console.log(x)
+
+        //for String
+let isLoggedIn = ""         //for empty string it gives false and when there is a character in string it gives true
+let x = Boolean(isLoggedIn)
+console.log(x)
